74ef2c10e746a687f1bbda9e6e0fc3c4669bfe57
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-03-26 11:07:21 +0530

    new files

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id
+
+  versioning_configuration {
+    status = var.versioning ? "Enabled" : "Suspended"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,7 @@
 variable "bucket_name" {}
+variable "environment" {}
+
+variable "versioning" {
+  type    = bool
+  default = true
+}
