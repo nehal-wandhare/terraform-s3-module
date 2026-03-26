f96e5bdb282a4aa1aa104333244bcffa9b3ad8da
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-03-26 19:49:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,15 @@
-variable "bucket_name" {}
-variable "environment" {}
+variable "bucket_name" {
+  description = "Base name of S3 bucket"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (dev/test/prod)"
+  type        = string
+}
 
 variable "versioning" {
-  type    = bool
-  default = true
+  description = "Enable versioning"
+  type        = bool
+  default     = true
 }
